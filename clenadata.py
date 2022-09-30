2c621953e2f1c794f127489e112310fefbcae9d4
Inês Pereirinha <56973954+inespereirinha@users.noreply.github.com>
2022-09-30 18:38:31 +0100

    Sentinel Values Missing

--- clenadata.py
+++ clenadata.py
@@ -2,28 +2,29 @@ from unittest import result
 import pandas as pd # Import the library
 import numpy as np
 
-# Read a comma-separated values (csv) file into DataFrame.
-bestseller_df = pd.read_csv("Project/Books_df.csv", encoding = 'latin1', delimiter=',')
-generalbooklist_df = pd.read_csv("Project/books_1.Best_Books_Ever.csv", encoding = 'utf-8', delimiter=',')
-new_bestseller_list = pd.read_csv("Project/amazon_bs_20102020.csv", encoding = 'utf-8', delimiter=',')
+#FIXME: clean up!
+#TODO: series -> sentinel values
 
-#bestseller_df.dropna(subset = ["Title"], inplace=True)
-#generalbooklist_df.dropna(subset = ["title"], inplace=True)
-#generalbooklist_df.dropna(subset = ["author"], inplace=True)
-#generalbooklist_df.dropna(subset = ["price"], inplace=True)
+# Read a comma-separated values (csv) file into DataFrame.
+#bestseller_df = pd.read_csv("Books_df.csv", encoding = 'latin1', delimiter=',')
+generalbooklist_df = pd.read_csv("books_1.Best_Books_Ever.csv", encoding = 'utf-8', delimiter=',')
+new_bestseller_list = pd.read_csv("amazon_bs_20102020.csv", encoding = 'utf-8', delimiter=',')
+
+generalbooklist_df.dropna(subset = ["title"], inplace=True)
+generalbooklist_df.dropna(subset = ["author"], inplace=True)
 
 # Sort and remove duplicates while keeping the first record.
-generalbooklist_df.sort_values("title", inplace = True)
+#generalbooklist_df.sort_values("title", inplace = True)
 #generalbooklist_df.drop_duplicates(subset ="title", keep = 'first', inplace = True)
 
-bestseller_df.sort_values("Title", inplace = True)
+#bestseller_df.sort_values("Title", inplace = True)
 #bestseller_df.drop_duplicates(subset ="Title", keep = 'first', inplace = True)
 
-new_bestseller_list.sort_values("Book_Title", inplace=True)
+#new_bestseller_list.sort_values("Book_Title", inplace=True)
 
 
 
-generalbooklist_df.drop(['bookId', 'description', 'isbn', 'characters', 'firstPublishDate', 'bbeScore', 'bbeVotes', 'coverImg'], axis = 1, inplace=True)
+generalbooklist_df.drop(['bookId', 'author', 'description', 'edition', 'isbn', 'characters', 'firstPublishDate', 'publishDate', 'bbeScore', 'bbeVotes', 'coverImg', 'price'], axis = 1, inplace=True)
 
 #generalbooklist_df['title'] = generalbooklist_df['title'].str.encode('windows-1252').str.decode('utf8')
 
@@ -31,14 +32,16 @@ generalbooklist_df.drop(['bookId', 'description', 'isbn', 'characters', 'firstPu
 
 #print(bestseller_df.count)
 
-results = pd.merge(generalbooklist_df, new_bestseller_list, how="inner", left_on='title', right_on='Book_Title')
+results = pd.merge(generalbooklist_df, new_bestseller_list, how="inner", left_on='title', right_on='Book_Title', sort = True)
 results.drop_duplicates(subset ="title", keep = 'first', inplace = True)
+results.drop(['Book_Title', 'Rank'], axis = 1, inplace=True)
 
-print(results.head)
+#print(results.head)
 
 #print(results.head())
 #print(bestseller_df.head())
 
-generalbooklist_df.to_csv('Project/finalData.csv', index = False, header = True)
+generalbooklist_df.to_csv('finalData.csv', index = False, header = True)
 
-results.to_csv('Project/merge.csv', index = False, header = True)
+results.to_csv('merge.csv', index = False, header = True)
+print(results.count())
