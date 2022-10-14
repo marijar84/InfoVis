ae4cfc7e1661d4a4d68ae492306c505bfa45d755
Maribel <marijar84@gmail.com>
2022-10-14 13:04:40 +0100

    Maribel --> Dropdown, scatterplot, interface

--- CP4/project.js
+++ CP4/project.js
@@ -1,5 +1,154 @@
+const csvdata = "";
+var allGenres = [];
 
 
-d3.csv("resultshandmade2.csv" , function(data) {
-    console.log(data);
+d3.csv("resultshandmade2.csv").then(function (data) {
+    filldropDown_genre(data);
 });
+
+
+
+/*************************************** Start --> Dropdown list ***************************************/
+//fill out dropdown list information
+function filldropDown_genre(data) {
+    for (var i = 0; i < data.length; i++) {
+        var genres = data[i].genres;
+        console.log(data[i]);
+        //Remove all special characteres    
+        genres = genres.replaceAll("[", "").replaceAll("]", "").replaceAll("'", "");
+        //set in an array allgenres by item      
+        var arrayGenre = genres.split(", ");
+
+        for (var j = 0; j < arrayGenre.length; j++) {
+            allGenres.push(arrayGenre[j]);
+        }
+    }
+    //Sort all genre
+    allGenres.sort();
+    const genreDropDown = document.getElementById("genres");
+
+    //Fill out information inside dropdown list
+    for (var i = 0; i < allGenres.length; i++) {
+        let option = document.createElement("option");
+        option.setAttribute("value", allGenres[i]);
+
+        var genreName = allGenres[i];
+        let optionText = document.createTextNode(genreName);
+        option.appendChild(optionText);
+
+        genreDropDown.appendChild(option);
+    }
+
+    removeduplicate();
+}
+
+//remove duplicate data for dropdown list
+function removeduplicate() {
+    var mycode = {};
+    $("select[id='genres'] > option").each(function () {
+        if (mycode[this.text]) {
+            $(this).remove();
+        } else {
+            mycode[this.text] = this.value;
+        }
+    });
+}
+
+/*************************************** End --> Dropdown list ***************************************/
+
+
+/*************************************** Start --> Slider by year ***************************************/
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+});
+
+/*************************************** End --> Slider by year ***************************************/
+
+/*************************************** Start --> Scatter plot ***************************************/
+
+function init2() {
+
+    var dataset1 = [
+        [90, 20], [20, 100], [66, 44],
+        [53, 80], [24, 182], [80, 72],
+        [10, 76], [33, 150], [100, 15]
+    ];
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // Step 4 
+    var xScale = d3.scaleLinear().domain([0, 100]).range([0, width]),
+        yScale = d3.scaleLinear().domain([0, 200]).range([height, 0]);
+
+    // Step 5
+    // Title
+    svg.append('text')
+        .attr('x', width / 2 + 100)
+        .attr('y', 100)
+        .attr('text-anchor', 'middle')
+        .style('font-family', 'Helvetica')
+        .style('font-size', 20)
+        .text('Scatter Plot');
+
+    // X label
+    svg.append('text')
+        .attr('x', width / 2 + 100)
+        .attr('y', height - 15 + 150)
+        .attr('text-anchor', 'middle')
+        .style('font-family', 'Helvetica')
+        .style('font-size', 12)
+        .text('Independant');
+
+    // Y label
+    svg.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('transform', 'translate(60,' + height + ')rotate(-90)')
+        .style('font-family', 'Helvetica')
+        .style('font-size', 12)
+        .text('Dependant');
+
+    // Step 6
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
+
+    // Step 7
+    svg.append('g')
+        .selectAll("dot")
+        .data(dataset1)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return xScale(d[0]); })
+        .attr("cy", function (d) { return yScale(d[1]); })
+        .attr("r", 2)
+        .attr("transform", "translate(" + 100 + "," + 100 + ")")
+        .style("fill", "#CC0000");
+
+    /*************************************** End --> Scatter plot ***************************************/
+}
