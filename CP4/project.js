801b753d138ccccf2be143fa38cd6b4757eff109
Maribel <marijar84@gmail.com>
2022-10-19 21:17:22 +0100

    MaribelJ
    PieChart

--- CP4/project.js
+++ CP4/project.js
@@ -1,23 +1,27 @@
-const csvdata = "";
+let csvdata;
 var allGenres = [];
 
 
 
 
-function init(){
+function init() {
     d3.csv("resultshandmade2.csv").then(function (data) {
         filldropDown_genre(data);
         scatterPlot(data);
-        sankeyChart(data);        
-        pieChart(data);
+        sankeyChart(data);
+        dataPieChart(data);
+        csvdata = data;
     });
+
+
 }
 
 /*************************************** Start --> Dropdown list ***************************************/
 //fill out dropdown list information
 function filldropDown_genre(data) {
+
     for (var i = 0; i < data.length; i++) {
-        var genres = data[i].genres;        
+        var genres = data[i].genres;
         //Remove all special characteres    
         genres = genres.replaceAll("[", "").replaceAll("]", "").replaceAll("'", "");
         //set in an array allgenres by item      
@@ -29,6 +33,7 @@ function filldropDown_genre(data) {
     }
     //Sort all genre
     allGenres.sort();
+    allGenres.unshift("All genre");
     const genreDropDown = document.getElementById("genres");
 
     //Fill out information inside dropdown list
@@ -58,6 +63,30 @@ function removeduplicate() {
     });
 }
 
+function selectDropDown(value) {
+    if (value == "All genre") {
+        removePieChart();
+        dataPieChart(csvdata);
+    }
+    else {
+        const filteredResult = csvdata.filter((item) => {
+            return (item.genres.indexOf(value) >= 0);
+        });
+
+        if (filteredResult.length > 0) {
+            removePieChart();
+            dataPieChart(filteredResult);
+        }
+    }
+}
+
+function removePieChart() {
+    var div = document.getElementById('pieChart');
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
 /*************************************** End --> Dropdown list ***************************************/
 
 
@@ -82,10 +111,18 @@ $(function () {
 /*************************************** Start --> Sankey chart ***************************************/
 
 function sankeyChart(data) {
-   var dataByYear = d3.group(data, d => d.publishDate);
+    var dataByYear = d3.group(data, d => d.publishDate);
 
-   //console.log(dataByYear);
+    //console.log(dataByYear);
 
+}
+
+function dataSankey(data) {
+
+    var data;
+
+
+    return data;
 }
 
 /*************************************** End --> Sankey chart ***************************************/
@@ -118,6 +155,15 @@ function scatterPlot(data) {
     var xScale = d3.scaleLinear().domain(d3.extent(nawards)).range([0, width])
     var yScale = d3.scaleLinear().domain([75, 100]).range([height, 0]);
 
+
+    max = d3.max(data, function (d) { return d.awards; })
+    console.log(max);
+
+    // Step 4 
+    var xScale = d3.scaleLinear().domain([0, 100]).range([0, width])
+    var yScale = d3.scaleLinear().domain([0, 200]).range([height, 0]);
+
+    // Step 5
     // Title
     svg.append('text')
         .attr('x', width / 2)
@@ -176,23 +222,188 @@ function scatterPlot(data) {
 
 /*************************************** Start --> Pie chart ***************************************/
 
-function pieChart(data){
+function pieChart(data, startYear, endYear) {
+
+    if (data.length > 0) {
+
+        var iDiv = document.createElement('div');
+        var nameDiv = startYear;
+        iDiv.innerHTML = nameDiv + "s";
+        iDiv.className = startYear
+
+        // set the dimensions and margins of the graph
+        var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+            width = 460 + margin.left + margin.right,
+            height = 400 + margin.top + margin.bottom;
+
+        // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+        var radius = 200;//Math.min(width, height) / 2 - margin
+
+        // append the svg object to the div called 'my_dataviz'
+        var svg = d3.select(iDiv)
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("background", "white")
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        // set the color scale
+        var color = d3.scaleOrdinal()
+            .domain(data)
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]);
+
+        // Compute the position of each group on the pie:
+        var pie = d3.pie()
+            .value(function (d) { return d.Amount; })(data);
+
+        var arcGenerator = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius)
+
+        svg
+            .selectAll('whatever')
+            .data(pie)
+            .enter()
+            .append('path')
+            .attr('d', arcGenerator)
+            .attr('fill', function (d) { return (color(d.data.Author)) })
+            .attr("stroke", "black")
+            .style("stroke-width", "2px")
+            .style("opacity", 0.7);
+
+        //Add Annotation
+        svg
+            .selectAll('mySlices')
+            .data(pie)
+            .enter()
+            .append('text')
+            .text(function (d) { return d.data.Author })
+            .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
+            .style("text-anchor", "middle")
+            .style("font-size", 17)
+
+        document.getElementById('pieChart').appendChild(iDiv);
+    }
+}
+
+function dataPieChart(data) {
+
+    var pieChartData;
+
     //Sort data by publishedData
-    data.sort(function(x, y){
+    data.sort(function (x, y) {
         return d3.ascending(x.publishDate, y.publishDate);
-     });    
-    
-    //GroupByYear
-    var dataByYear = d3.group(data, d => d.publishDate);
-       
-    console.log(dataByYear);
+    });
 
-    /*var iDiv = document.createElement('div');
-    iDiv.id = 'block';
-    iDiv.className = 'block';
-    document.getElementsByTagName('pieChart')[0].appendChild(iDiv);*/
+    //Remove missing values
+    var dataFilter = data.filter(function (d) { return d.publishDate !== "Missing" });
 
+    var isFinished = false;
+    var startYear = parseInt(dataFilter[0].publishDate.slice(0, -1) + '0');
 
+    var lastYearPosition = dataFilter.length - 1;
+    var lastYear = dataFilter[lastYearPosition].publishDate;
+
+    for (var i = startYear; i <= lastYear; i = i + 10) {
+
+        var endYear = parseInt(startYear + 10);
+        console.log("startYear", startYear, "endYear", endYear, "lastYear", lastYear, "i ", i);
+
+        //GroupByYear 
+        pieChartData = dataFilter.filter(function (d) { return d.publishDate >= startYear && d.publishDate < endYear });
+        pieChart(getAuthorByBook(pieChartData), startYear, endYear);
+
+        startYear = endYear;
+
+    }
+    return pieChartData;
+}
+
+function getAuthorByBook(data) {
+    //Remove missing values
+    var dataFilter = data.filter(function (d) { return d.Author !== "Missing" });
+
+    var authors = d3.group(dataFilter, d => d.Author);
+    //var dataFilterAuthorOneBook = authors.filter((item) => {
+    //  return (item.value.indexOf(value) > 1)});
+    var pieData = [];
+    var authorByBook = [];
+
+    authors.forEach((valueAuthor, keyAuthor) => {
+
+        var amount = 1;
+        var books = new Array();
+        var authorName = "";
+
+        console.log("authors", authors.size);
+        if (authors.size > 30) {
+            if (valueAuthor.length == 1) {
+                authorName = "Authors";
+                var index = pieData.filter(function (x) { return x.Author == authorName });
+
+                if (index.length == 0) {
+                    authorByBook.push({
+                        Author: valueAuthor[0].Author,
+                        Book: valueAuthor[0].title
+                    });
+
+                    pieData.push({
+                        Author: authorName,
+                        Amount: amount,
+                        Books: books,
+                        AuthorByBook: authorByBook,
+                    });
+                }
+                else {
+                    index[0].Amount = index[0].Amount + 1;
+
+                    authorByBook = index[0].AuthorByBook;
+                    authorByBook.push({
+                        Author: valueAuthor[0].Author,
+                        Book: valueAuthor[0].title
+                    });
+                }
+            }
+            else {
+
+                valueAuthor.forEach((valueAuthor1, keyAuthor1) => {
+
+                    authorName = valueAuthor1.Author;
+
+                    //if (amount <= valueAuthor.length) {
+                    books.push(valueAuthor1.title);
+                    //}
+                    amount = valueAuthor.length;
+                });
+                pieData.push({
+                    Author: authorName,
+                    Amount: amount,
+                    Books: books,
+                    AuthorByBook: authorByBook,
+                });
+            }
+        }
+        else {
+            valueAuthor.forEach((valueAuthor1, keyAuthor1) => {
+
+                authorName = valueAuthor1.Author;
+
+                //if (amount <= valueAuthor.length) {
+                books.push(valueAuthor1.title);
+                //}
+                amount = valueAuthor.length;
+            });
+            pieData.push({
+                Author: authorName,
+                Amount: amount,
+                Books: books,
+                AuthorByBook: authorByBook,
+            });
+        }
+    });
+    console.log("pieData", pieData);
+    return pieData;
 }
 
 
